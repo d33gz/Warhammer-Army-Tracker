266df1c78d1b8bca23a241a71715a2b2c294cc2a
Steven Laptop <sdhdh2@gmail.com>
2021-03-25 09:31:41 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Template for making basic HTML, CSS, and JavaScript Websites. Includes a folder for storing images. The only file with any content is this README and the index.html file.
+This app tracks a User's Warhammer Armies. It allows for the logging of information about a particular Faction and the point cost/setup of each of it's units. Use it for tracking your armies or building out dream teams. It also allows a User to specifiy paint colors, the amount of coats to apply, and where the paint goes.
---
+++ script.js
@@ -0,0 +1,20 @@
+//Sample Code ideas to use for tracking Figurines.
+
+//The MasterFigure Object is a single Object that is used as a template for creating more Objects per Unit
+  /*
+    let masterFigure = {
+      name: "",
+      quantity: null,
+      gear: [],
+      stats: [],
+      cost: null,
+      paint_scheme: []
+    };
+  */
+  
+  //name is for the name of the figurine
+  //quantity is how many of a figurine a User has
+  //gear is an array for tracking each piece of gear (as an object with name/cost/stats/etc)
+  //stats is for tracking each stat on a unit (ex. WS,BS,S,T,etc)
+  //cost is the total points cost for a given model after factoring in how many and it's gear
+  //paint_scheme is used to track which paints were used where and how much paint was appplied
